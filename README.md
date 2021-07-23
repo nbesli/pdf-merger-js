46d16e44b93e2338db0dc0a6e2964c612ed82b49
DanMHammer <35697323+DanMHammer@users.noreply.github.com>
2021-07-23 09:53:43 -0500

    Make Browser compatible version with Promises and FileReader API

--- README.md
+++ README.md
@@ -11,24 +11,66 @@ This library is inspired by the [PHP library PDFMerger](https://github.com/myoky
 ## Code sample
 
 ```javascript
-const PDFMerger = require('pdf-merger-js');
+const PDFMerger = require("pdf-merger-js");
 
 var merger = new PDFMerger();
 
 (async () => {
-  merger.add('pdf1.pdf');  //merge all pages. parameter is the path to file and filename.
-  merger.add('pdf2.pdf', [2]); // merge only page 2
-  merger.add('pdf2.pdf', [1, 3]); // merge the pages 1 and 3
-  merger.add('pdf2.pdf', '4, 7, 8'); // merge the pages 4, 7 and 8
-  merger.add('pdf3.pdf', '1 to 2'); //merge pages 1 to 2
-  merger.add('pdf3.pdf', '3-4'); //merge pages 3 to 4
+  merger.add("pdf1.pdf"); //merge all pages. parameter is the path to file and filename.
+  merger.add("pdf2.pdf", [2]); // merge only page 2
+  merger.add("pdf2.pdf", [1, 3]); // merge the pages 1 and 3
+  merger.add("pdf2.pdf", "4, 7, 8"); // merge the pages 4, 7 and 8
+  merger.add("pdf3.pdf", "1 to 2"); //merge pages 1 to 2
+  merger.add("pdf3.pdf", "3-4"); //merge pages 3 to 4
 
-  await merger.save('merged.pdf'); //save under given name and reset the internal document
+  await merger.save("merged.pdf"); //save under given name and reset the internal document
 })();
+```
+
+### Browser Sample - React
+
+```javascript
+import PDFMerger from "pdf-merger-js/browser";
+import React, { useEffect, useState } from "react";
+
+// files: Array of PDF File or Blob objects
+const Merger = (files) => {
+  const [mergedPdfUrl, setMergedPdfUrl] = useState();
+
+  useEffect(() => {
+    const render = async () => {
+      const merger = new PDFMerger();
+
+      await Promise.all(files.map(async (file) => await merger.add(file)));
+
+      const mergedPdf = await merger.saveAsBlob();
+      const url = URL.createObjectURL(mergedPdf);
+
+      return setMergedPdfUrl(url);
+    };
+
+    render().catch((err) => {
+      throw err;
+    });
+
+    () => setMergedPdfUrl({});
+  }, [files, setMergedPdfUrl]);
+
+  return !data ? (
+    <>Loading</>
+  ) : (
+    <iframe
+      height={1000}
+      src={`${mergedPdfUrl}`}
+      title="pdf-viewer"
+      width="100%s"
+    ></iframe>
+  );
+};
 ```
 
 ## Similar libraries
 
-* [pdf-merge](https://www.npmjs.com/package/pdf-merge) has a dependency on [PDFtk](https://www.pdflabs.com/tools/pdftk-the-pdf-toolkit/).
-* [easy-pdf-merge](https://www.npmjs.com/package/easy-pdf-merge) has a dependency on the [Apache PDFBox® - A Java PDF Library](https://pdfbox.apache.org/).
-* [pdfmerge](https://www.npmjs.com/package/pdfmerge) has a dependency on python and [PyPDF2](https://pythonhosted.org/PyPDF2/).
+- [pdf-merge](https://www.npmjs.com/package/pdf-merge) has a dependency on [PDFtk](https://www.pdflabs.com/tools/pdftk-the-pdf-toolkit/).
+- [easy-pdf-merge](https://www.npmjs.com/package/easy-pdf-merge) has a dependency on the [Apache PDFBox® - A Java PDF Library](https://pdfbox.apache.org/).
+- [pdfmerge](https://www.npmjs.com/package/pdfmerge) has a dependency on python and [PyPDF2](https://pythonhosted.org/PyPDF2/).
--- test/fixtures.test.js
+++ test/fixtures.test.js
@@ -1,94 +1,94 @@
-const path = require('path')
-const fs = require('fs-extra')
-const pdfDiff = require('pdf-diff')
+const path = require("path");
+const fs = require("fs-extra");
+const pdfDiff = require("pdf-diff");
 
-const PDFMerger = require('../index')
+const PDFMerger = require("../index");
 
-const FIXTURES_DIR = path.join(__dirname, 'fixtures')
-const TMP_DIR = path.join(__dirname, 'tmp')
+const FIXTURES_DIR = path.join(__dirname, "fixtures");
+const TMP_DIR = path.join(__dirname, "tmp");
 
-jest.setTimeout(10000)
+jest.setTimeout(10000);
 
-describe('PDFMerger', () => {
+describe("PDFMerger", () => {
   beforeAll(async () => {
-    await fs.ensureDir(TMP_DIR)
-  })
+    await fs.ensureDir(TMP_DIR);
+  });
 
-  test('merge two simple files', async () => {
-    const merger = new PDFMerger()
-    merger.add(path.join(FIXTURES_DIR, 'Testfile_A.pdf'))
-    merger.add(path.join(FIXTURES_DIR, 'Testfile_B.pdf'))
-    await merger.save(path.join(TMP_DIR, 'Testfile_AB.pdf'))
-
-    const diff = await pdfDiff(
-      path.join(FIXTURES_DIR, 'Testfile_AB.pdf'),
-      path.join(TMP_DIR, 'Testfile_AB.pdf')
-    )
-
-    expect(diff).toBeFalsy()
-  })
-
-  test('combine pages from multibe books (array)', async () => {
-    const merger = new PDFMerger()
-    const tmpFile = 'MergeDemo1.pdf'
-    merger.add(path.join(FIXTURES_DIR, 'Testfile_AB.pdf'), [1])
-    merger.add(path.join(FIXTURES_DIR, 'UDHR.pdf'), [1, 2, 3])
-    await merger.save(path.join(TMP_DIR, tmpFile))
+  test("merge two simple files", async () => {
+    const merger = new PDFMerger();
+    merger.add(path.join(FIXTURES_DIR, "Testfile_A.pdf"));
+    merger.add(path.join(FIXTURES_DIR, "Testfile_B.pdf"));
+    await merger.save(path.join(TMP_DIR, "Testfile_AB.pdf"));
 
     const diff = await pdfDiff(
-      path.join(FIXTURES_DIR, 'MergeDemo.pdf'),
+      path.join(FIXTURES_DIR, "Testfile_AB.pdf"),
+      path.join(TMP_DIR, "Testfile_AB.pdf")
+    );
+
+    expect(diff).toBeFalsy();
+  });
+
+  test("combine pages from multibe books (array)", async () => {
+    const merger = new PDFMerger();
+    const tmpFile = "MergeDemo1.pdf";
+    merger.add(path.join(FIXTURES_DIR, "Testfile_AB.pdf"), [1]);
+    merger.add(path.join(FIXTURES_DIR, "UDHR.pdf"), [1, 2, 3]);
+    await merger.save(path.join(TMP_DIR, tmpFile));
+
+    const diff = await pdfDiff(
+      path.join(FIXTURES_DIR, "MergeDemo.pdf"),
       path.join(TMP_DIR, tmpFile)
-    )
+    );
 
-    expect(diff).toBeFalsy()
-  })
+    expect(diff).toBeFalsy();
+  });
 
-  test('combine pages from multibe books (start-end)', async () => {
-    const merger = new PDFMerger()
-    const tmpFile = 'MergeDemo2.pdf'
-    merger.add(path.join(FIXTURES_DIR, 'Testfile_AB.pdf'), [1])
-    merger.add(path.join(FIXTURES_DIR, 'UDHR.pdf'), '1-3')
-    await merger.save(path.join(TMP_DIR, tmpFile))
-
-    const diff = await pdfDiff(
-      path.join(FIXTURES_DIR, 'MergeDemo.pdf'),
-      path.join(TMP_DIR, tmpFile)
-    )
-
-    expect(diff).toBeFalsy()
-  })
-
-  test('combine pages from multibe books (start - end)', async () => {
-    const merger = new PDFMerger()
-    const tmpFile = 'MergeDemo2.pdf'
-    merger.add(path.join(FIXTURES_DIR, 'Testfile_AB.pdf'), [1])
-    merger.add(path.join(FIXTURES_DIR, 'UDHR.pdf'), '1 - 3')
-    await merger.save(path.join(TMP_DIR, tmpFile))
+  test("combine pages from multibe books (start-end)", async () => {
+    const merger = new PDFMerger();
+    const tmpFile = "MergeDemo2.pdf";
+    merger.add(path.join(FIXTURES_DIR, "Testfile_AB.pdf"), [1]);
+    merger.add(path.join(FIXTURES_DIR, "UDHR.pdf"), "1-3");
+    await merger.save(path.join(TMP_DIR, tmpFile));
 
     const diff = await pdfDiff(
-      path.join(FIXTURES_DIR, 'MergeDemo.pdf'),
+      path.join(FIXTURES_DIR, "MergeDemo.pdf"),
       path.join(TMP_DIR, tmpFile)
-    )
+    );
 
-    expect(diff).toBeFalsy()
-  })
+    expect(diff).toBeFalsy();
+  });
 
-  test('combine pages from multibe books (start to end)', async () => {
-    const merger = new PDFMerger()
-    const tmpFile = 'MergeDemo2.pdf'
-    merger.add(path.join(FIXTURES_DIR, 'Testfile_AB.pdf'), [1])
-    merger.add(path.join(FIXTURES_DIR, 'UDHR.pdf'), '1 to 3')
-    await merger.save(path.join(TMP_DIR, tmpFile))
+  test("combine pages from multibe books (start - end)", async () => {
+    const merger = new PDFMerger();
+    const tmpFile = "MergeDemo2.pdf";
+    merger.add(path.join(FIXTURES_DIR, "Testfile_AB.pdf"), [1]);
+    merger.add(path.join(FIXTURES_DIR, "UDHR.pdf"), "1 - 3");
+    await merger.save(path.join(TMP_DIR, tmpFile));
 
     const diff = await pdfDiff(
-      path.join(FIXTURES_DIR, 'MergeDemo.pdf'),
+      path.join(FIXTURES_DIR, "MergeDemo.pdf"),
       path.join(TMP_DIR, tmpFile)
-    )
+    );
 
-    expect(diff).toBeFalsy()
-  })
+    expect(diff).toBeFalsy();
+  });
+
+  test("combine pages from multibe books (start to end)", async () => {
+    const merger = new PDFMerger();
+    const tmpFile = "MergeDemo2.pdf";
+    merger.add(path.join(FIXTURES_DIR, "Testfile_AB.pdf"), [1]);
+    merger.add(path.join(FIXTURES_DIR, "UDHR.pdf"), "1 to 3");
+    await merger.save(path.join(TMP_DIR, tmpFile));
+
+    const diff = await pdfDiff(
+      path.join(FIXTURES_DIR, "MergeDemo.pdf"),
+      path.join(TMP_DIR, tmpFile)
+    );
+
+    expect(diff).toBeFalsy();
+  });
 
   afterAll(async () => {
-    await fs.remove(TMP_DIR)
-  })
-})
+    await fs.remove(TMP_DIR);
+  });
+});
