ad73cf2f76526e9b20c445b32f96c686f431ceb0
DanMHammer <35697323+DanMHammer@users.noreply.github.com>
2021-07-26 10:13:10 -0500

    use window.Blob and window.FileReader to satisfy 'standard' requirements

--- browser.js
+++ browser.js
@@ -1,127 +1,127 @@
-const pdf = require('pdfjs');
+const pdf = require('pdfjs')
 
 class PDFMerger {
-  constructor() {
-    this._resetDoc();
-    console.log('linked version');
+  constructor () {
+    this._resetDoc()
+    console.log('linked version')
   }
 
-  add(inputFile, pages) {
+  add (inputFile, pages) {
     if (typeof pages === 'undefined' || pages === null) {
-      return this._addEntireDocument(inputFile, pages);
+      return this._addEntireDocument(inputFile, pages)
     } else if (Array.isArray(pages)) {
-      return this._addGivenPages(inputFile, pages);
+      return this._addGivenPages(inputFile, pages)
     } else if (pages.indexOf(',') > 0) {
-      return this._addGivenPages(inputFile, pages.replace(/ /g, '').split(','));
+      return this._addGivenPages(inputFile, pages.replace(/ /g, '').split(','))
     } else if (pages.toLowerCase().indexOf('to') >= 0) {
-      const span = pages.replace(/ /g, '').split('to');
+      const span = pages.replace(/ /g, '').split('to')
       return this._addFromToPage(
         inputFile,
         parseInt(span[0]),
         parseInt(span[1])
-      );
+      )
     } else if (pages.indexOf('-') >= 0) {
-      const span = pages.replace(/ /g, '').split('-');
+      const span = pages.replace(/ /g, '').split('-')
       return this._addFromToPage(
         inputFile,
         parseInt(span[0]),
         parseInt(span[1])
-      );
+      )
     } else {
-      console.error('invalid parameter "pages"');
+      console.error('invalid parameter "pages"')
     }
   }
 
-  _resetDoc() {
+  _resetDoc () {
     if (this.doc) {
-      delete this.doc;
+      delete this.doc
     }
-    this.doc = new pdf.Document();
+    this.doc = new pdf.Document()
   }
 
-  async _getInputFile(inputFile) {
+  async _getInputFile (inputFile) {
     return new Promise((resolve) => {
       if (inputFile instanceof Buffer) {
-        resolve(inputFile);
+        resolve(inputFile)
       } else {
-        const fileReader = new FileReader();
+        const fileReader = new window.FileReader()
 
         fileReader.onload = function (evt) {
-          resolve(fileReader.result);
-        };
+          resolve(fileReader.result)
+        }
 
-        fileReader.readAsArrayBuffer(inputFile);
+        fileReader.readAsArrayBuffer(inputFile)
       }
-    });
+    })
   }
 
-  async _addEntireDocument(inputFile) {
-    const src = await this._getInputFile(inputFile);
-    console.log(src);
-    const ext = new pdf.ExternalDocument(src);
+  async _addEntireDocument (inputFile) {
+    const src = await this._getInputFile(inputFile)
+    console.log(src)
+    const ext = new pdf.ExternalDocument(src)
 
-    return this.doc.addPagesOf(ext);
+    return this.doc.addPagesOf(ext)
   }
 
-  async _addFromToPage(inputFile, from, to) {
+  async _addFromToPage (inputFile, from, to) {
     if (
       typeof from === 'number' &&
       typeof to === 'number' &&
       from > 0 &&
       to > from
     ) {
-      let pages = [];
+      const pages = []
 
       for (let i = from; i <= to; i++) {
-        pages.push(i);
+        pages.push(i)
       }
 
-      const src = await this._getInputFile(inputFile);
-      const ext = new pdf.ExternalDocument(src);
-      this.doc.setTemplate(ext);
+      const src = await this._getInputFile(inputFile)
+      const ext = new pdf.ExternalDocument(src)
+      this.doc.setTemplate(ext)
 
       return Promise.all(
         pages.map(async (page) => this.doc.addPageOf(page, ext))
-      );
+      )
     } else {
-      console.log('invalid function parameter');
+      console.log('invalid function parameter')
     }
   }
 
-  async _addGivenPages(inputFile, pages) {
+  async _addGivenPages (inputFile, pages) {
     if (pages.length > 0) {
-      const src = await this._getInputFile(inputFile);
-      const ext = new pdf.ExternalDocument(src);
-      this.doc.setTemplate(ext);
+      const src = await this._getInputFile(inputFile)
+      const ext = new pdf.ExternalDocument(src)
+      this.doc.setTemplate(ext)
 
       return Promise.all(
         pages.map(async (page) => {
-          this.doc.addPageOf(page, ext);
+          this.doc.addPageOf(page, ext)
         })
-      );
+      )
     }
   }
 
-  async saveAsBuffer() {
-    return this.doc.asBuffer();
+  async saveAsBuffer () {
+    return this.doc.asBuffer()
   }
 
-  async saveAsBlob() {
-    const buffer = await this.saveAsBuffer();
+  async saveAsBlob () {
+    const buffer = await this.saveAsBuffer()
 
-    return new Blob([buffer], {
-      type: `application/pdf`,
-    });
+    return new window.Blob([buffer], {
+      type: 'application/pdf'
+    })
   }
 
-  async save(fileName) {
-    const blob = await this.saveAsBlob();
+  async save (fileName) {
+    const blob = await this.saveAsBlob()
 
-    const link = document.createElement('a');
-    link.href = window.URL.createObjectURL(blob);
-    link.download = `${fileName}.pdf`;
-    link.click();
+    const link = document.createElement('a')
+    link.href = window.URL.createObjectURL(blob)
+    link.download = `${fileName}.pdf`
+    link.click()
   }
 }
 
-module.exports = PDFMerger;
+module.exports = PDFMerger
--- test/browser-fixtures.test.js
+++ test/browser-fixtures.test.js
@@ -1,145 +1,145 @@
-const path = require('path');
-const fs = require('fs-extra');
-const pdfDiff = require('pdf-diff');
+const path = require('path')
+const fs = require('fs-extra')
+const pdfDiff = require('pdf-diff')
 
-const PDFMerger = require('../browser');
+const PDFMerger = require('../browser')
 
-const FIXTURES_DIR = path.join(__dirname, 'fixtures');
-const TMP_DIR = path.join(__dirname, 'tmp');
+const FIXTURES_DIR = path.join(__dirname, 'fixtures')
+const TMP_DIR = path.join(__dirname, 'tmp')
 
-jest.setTimeout(10000);
+jest.setTimeout(10000)
 
 // Note: The browser tests differ from standard as all files are expected
 // to be generated or fetched before being passed into the merger.
 // For testing, they are retrieved with fs.ReadFile() and then passed in.
 describe('PDFMerger', () => {
   beforeAll(async () => {
-    await fs.ensureDir(TMP_DIR);
-  });
+    await fs.ensureDir(TMP_DIR)
+  })
 
   test('merge two simple files', async () => {
-    const merger = new PDFMerger();
+    const merger = new PDFMerger()
 
     await merger.add(
       await fs.readFile(path.join(FIXTURES_DIR, 'Testfile_A.pdf'))
-    );
+    )
     await merger.add(
       await fs.readFile(path.join(FIXTURES_DIR, 'Testfile_B.pdf'))
-    );
+    )
 
-    const buffer = await merger.saveAsBuffer();
+    const buffer = await merger.saveAsBuffer()
     // Write the buffer as a file for pdfDiff
-    await fs.writeFile(path.join(TMP_DIR, 'Testfile_AB.pdf'), buffer);
+    await fs.writeFile(path.join(TMP_DIR, 'Testfile_AB.pdf'), buffer)
 
     const diff = await pdfDiff(
       path.join(FIXTURES_DIR, 'Testfile_AB.pdf'),
       path.join(TMP_DIR, 'Testfile_AB.pdf')
-    );
+    )
 
-    expect(diff).toBeFalsy();
-  });
+    expect(diff).toBeFalsy()
+  })
 
   test('combine pages from multiple books (array)', async () => {
-    const merger = new PDFMerger();
-    const tmpFile = 'MergeDemo1.pdf';
+    const merger = new PDFMerger()
+    const tmpFile = 'MergeDemo1.pdf'
 
     await merger.add(
       await fs.readFile(path.join(FIXTURES_DIR, 'Testfile_AB.pdf')),
       [1]
-    );
+    )
     await merger.add(
       await fs.readFile(path.join(FIXTURES_DIR, 'UDHR.pdf')),
       [1, 2, 3]
-    );
+    )
 
-    const buffer = await merger.saveAsBuffer();
+    const buffer = await merger.saveAsBuffer()
     // Write the buffer as a file for pdfDiff
-    await fs.writeFile(path.join(TMP_DIR, tmpFile), buffer);
+    await fs.writeFile(path.join(TMP_DIR, tmpFile), buffer)
 
     const diff = await pdfDiff(
       path.join(FIXTURES_DIR, 'MergeDemo.pdf'),
       path.join(TMP_DIR, tmpFile)
-    );
+    )
 
-    expect(diff).toBeFalsy();
-  });
+    expect(diff).toBeFalsy()
+  })
 
   test('combine pages from multiple books (start-end)', async () => {
-    const merger = new PDFMerger();
-    const tmpFile = 'MergeDemo2.pdf';
+    const merger = new PDFMerger()
+    const tmpFile = 'MergeDemo2.pdf'
 
     await merger.add(
       await fs.readFile(path.join(FIXTURES_DIR, 'Testfile_AB.pdf')),
       [1]
-    );
+    )
     await merger.add(
       await fs.readFile(path.join(FIXTURES_DIR, 'UDHR.pdf')),
       '1-3'
-    );
+    )
 
-    const buffer = await merger.saveAsBuffer();
+    const buffer = await merger.saveAsBuffer()
     // Write the buffer as a file for pdfDiff
-    await fs.writeFile(path.join(TMP_DIR, tmpFile), buffer);
+    await fs.writeFile(path.join(TMP_DIR, tmpFile), buffer)
 
     const diff = await pdfDiff(
       path.join(FIXTURES_DIR, 'MergeDemo.pdf'),
       path.join(TMP_DIR, tmpFile)
-    );
+    )
 
-    expect(diff).toBeFalsy();
-  });
+    expect(diff).toBeFalsy()
+  })
 
   test('combine pages from multiple books (start - end)', async () => {
-    const merger = new PDFMerger();
-    const tmpFile = 'MergeDemo2.pdf';
+    const merger = new PDFMerger()
+    const tmpFile = 'MergeDemo2.pdf'
 
     await merger.add(
       await fs.readFile(path.join(FIXTURES_DIR, 'Testfile_AB.pdf')),
       [1]
-    );
+    )
     await merger.add(
       await fs.readFile(path.join(FIXTURES_DIR, 'UDHR.pdf')),
       '1 - 3'
-    );
+    )
 
-    const buffer = await merger.saveAsBuffer();
+    const buffer = await merger.saveAsBuffer()
     // Write the buffer as a file for pdfDiff
-    await fs.writeFile(path.join(TMP_DIR, tmpFile), buffer);
+    await fs.writeFile(path.join(TMP_DIR, tmpFile), buffer)
 
     const diff = await pdfDiff(
       path.join(FIXTURES_DIR, 'MergeDemo.pdf'),
       path.join(TMP_DIR, tmpFile)
-    );
+    )
 
-    expect(diff).toBeFalsy();
-  });
+    expect(diff).toBeFalsy()
+  })
 
   test('combine pages from multiplee books (start to end)', async () => {
-    const merger = new PDFMerger();
-    const tmpFile = 'MergeDemo2.pdf';
+    const merger = new PDFMerger()
+    const tmpFile = 'MergeDemo2.pdf'
 
     await merger.add(
       await fs.readFile(path.join(FIXTURES_DIR, 'Testfile_AB.pdf')),
       [1]
-    );
+    )
     await merger.add(
       await fs.readFile(path.join(FIXTURES_DIR, 'UDHR.pdf')),
       '1 to 3'
-    );
+    )
 
-    const buffer = await merger.saveAsBuffer();
+    const buffer = await merger.saveAsBuffer()
     // Write the buffer as a file for pdfDiff
-    await fs.writeFile(path.join(TMP_DIR, tmpFile), buffer);
+    await fs.writeFile(path.join(TMP_DIR, tmpFile), buffer)
 
     const diff = await pdfDiff(
       path.join(FIXTURES_DIR, 'MergeDemo.pdf'),
       path.join(TMP_DIR, tmpFile)
-    );
+    )
 
-    expect(diff).toBeFalsy();
-  });
+    expect(diff).toBeFalsy()
+  })
 
   afterAll(async () => {
-    await fs.remove(TMP_DIR);
-  });
-});
+    await fs.remove(TMP_DIR)
+  })
+})
