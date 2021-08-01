24098397fb9364cb89628f38477c17420e6758aa
Alexander Wunschik <alexander.wunschik@fullstax.de>
2021-08-01 12:06:25 +0200

    chore: update browser types

--- browser.d.ts
+++ browser.d.ts
@@ -1,15 +1,16 @@
-// Type definitions for pdf-merger-js v3.0.1
+// Type definitions for pdf-merger-js v3.2.0
 // Project: https://github.com/nbesli/pdf-merger-js
 // Definitions by: Alexander Wunschik <https://github.com/mojoaxel/>
 //                 Daniel Hammer <https://github.com/danmhammer/>
-class PDFMerger {
-  constructor();
-  add(inputFile: string | ArrayBuffer | Blob | Buffer | File, pages?: string | string[] | undefined | null): Promise<undefined>;
-  save(fileName: string): Promise<undefined>;
-  saveAsBuffer(): Promise<Buffer>;
-  saveAsBlob(): Promise<Blob>;
-}
 
 declare module "pdf-merger-js/browser" {
+  class PDFMerger {
+    constructor();
+    add(inputFile: string | ArrayBuffer | Blob | Buffer | File, pages?: string | string[] | undefined | null): Promise<undefined>;
+    save(fileName: string): Promise<undefined>;
+    saveAsBuffer(): Promise<Buffer>;
+    saveAsBlob(): Promise<Blob>;
+  }
+
   export = PDFMerger;
 }
