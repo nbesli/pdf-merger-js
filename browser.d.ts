5623a60d3e81f6992095cfaff2687a8b297b5772
Alexander Wunschik <alex@wunschik.net>
2023-12-27 15:19:17 +0100

    feat: adopt typescript declarations to new implementation

--- browser.d.ts
+++ browser.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for pdf-merger-js v4.2.0
+// Type definitions for pdf-merger-js v5.0.0
 // Project: https://github.com/nbesli/pdf-merger-js
 // Definitions by: Alexander Wunschik <https://github.com/mojoaxel/>
 //                 Daniel Hammer <https://github.com/danmhammer/>
@@ -7,15 +7,58 @@
 declare module "pdf-merger-js/browser" {
   class PDFMerger {
     constructor();
-    add(inputFile: string | Uint8Array | ArrayBuffer | Blob | File, pages?: string | string[] | undefined | null): Promise<undefined>;
-    save(fileName: string): Promise<undefined>;
+    /**
+     * Resets the internal state of the document, to start again.
+     *
+     * @returns {void}
+     */
+    reset(): void;
+    /**
+     * Add pages from a PDF document to the end of the merged document.
+     *
+     * @async
+     * @param {PdfInput} input - a pdf source
+     * @param {string | string[] | number | number[] | undefined | null} [pages]
+     * @returns {Promise<void>}
+     */
+    add(inputFile: PdfInput, pages?: string | string[] | number | number[] | undefined | null): Promise<undefined>;
+    /**
+     * Download the PDF as a file with the given name.
+     * The extension ".pdf" is appended automatically.
+     *
+     * @async
+     * @param {string} fileName
+     * @returns {Promise<void>}
+     */
+    save(fileName: string): Promise<void>;
+    /**
+     * Return the merged PDF as a Uint8Array.
+     *
+     * @async
+     * @returns {Promise<Uint8Array>}
+     */
     saveAsBuffer(): Promise<Uint8Array>;
+    /**
+     * Return the merged PDF as a Blob.
+     *
+     * @async
+     * @returns {Promise<Blob>}
+     */
     saveAsBlob(): Promise<Blob>;
+    /**
+     * Set the metadata of the merged PDF.
+     *
+     * @async
+     * @param {Metadata} metadata
+     * @returns {Promise<void>}
+     */
     setMetadata(metadata: Metadata): Promise<void>;
   }
 
   export = PDFMerger;
 }
+
+declare type PdfInput = Uint8Array | ArrayBuffer | Blob | URL | File | String | string;
 
 declare interface Metadata {
   producer?: string
