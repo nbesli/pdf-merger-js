e6bf3958bf4c4b8b41c04b7e11def0de531e1ee2
Alexander Wunschik <alex@wunschik.net>
2023-12-27 15:19:17 +0100

    feat: adopt typescript declarations to new implementation

--- index.d.ts
+++ index.d.ts
@@ -1,19 +1,55 @@
-// Type definitions for pdf-merger-js v4.2.1+
+// Type definitions for pdf-merger-js v5.0.0
 // Project: https://github.com/nbesli/pdf-merger-js
 // Definitions by: Alexander Wunschik <https://github.com/mojoaxel/>
+
+import { PathLike } from "fs-extra";
 
 declare module "pdf-merger-js" {
   class PDFMerger {
     constructor();
+    /**
+     * Resets the internal state of the document, to start again.
+     *
+     * @returns {void}
+     */
     reset(): void;
-    add(inputFile: string | Buffer | ArrayBuffer, pages?: string | string[] | undefined | null): Promise<undefined>;
-    save(fileName: string): Promise<undefined>;
+    /**
+     * Add pages from a PDF document to the end of the merged document.
+     *
+     * @async
+     * @param {PdfInput} input - a pdf source
+     * @param {string | string[] | number | number[] | undefined | null} [pages]
+     * @returns {Promise<void>}
+     */
+    add(inputFile: PdfInput, pages?: string | string[] | number | number[] | undefined | null): Promise<void>;
+    /**
+     * Save the merged PDF to the given path.
+     *
+     * @async
+     * @param {string | PathLike} fileName
+     * @returns {Promise<void>}
+     */
+    save(fileName: string): Promise<void>;
+    /**
+     * Return the merged PDF as a Buffer.
+     *
+     * @async
+     * @returns {Promise<Buffer>}
+     */
     saveAsBuffer(): Promise<Buffer>;
+    /**
+     * Set the metadata of the merged PDF.
+     *
+     * @async
+     * @param {Metadata} metadata
+     * @returns {Promise<void>}
+     */
     setMetadata(metadata: Metadata): Promise<void>;
   }
   export = PDFMerger;
 }
 
+declare type PdfInput = Uint8Array | ArrayBuffer | Blob | URL | Buffer | String | PathLike | string;
 
 declare interface Metadata {
   producer?: string
